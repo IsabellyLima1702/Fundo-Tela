9672dbe875dfe4e2e10203510438175edb215659
ISABELLY LIMA DA SILVA <24122387@CFP127ALUNO.LOCAL>
2025-01-28 11:34:19 -0300

    js ok

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict"
+//"use strict" serve para corrigir os erros básicos
+
+const botaoTrocarImagem = document.getElementById('trocar-imagem')
+
+function trocarImagem () {
+
+    const imagem = document.getElementById('imagem').value
+    
+    document.documentElement.style.setProperty('--imagem-fundo', imagem)
+}
+botaoTrocarImagem.addEventListener('click', trocarImagem)
